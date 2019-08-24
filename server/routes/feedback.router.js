f7dd1647932c7d4f2cf3cb2e987c81094ac05a87
Andy DuBois <andydubois89@gmail.com>
2019-08-24 17:49:57 -0500

    setting up server side POST route now

---
+++ server/routes/feedback.router.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const pool = require("../modules/pool");
+
+
+
+
+
+router.post('/', (req, res))
+
+
+
+
+module.exports = router;
+
+
--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,14 +1,43 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import axios from 'axios';
 
 class Review extends Component {
-    render() {
-        return (
-            <div>
-                <h1>REVIEW PAGE BITCHES</h1>
-            </div>
-        )
+  state = {
+    feedback: {
+      feelings: this.props.reduxStore.feelingsReducer,
+      understanding: this.props.reduxStore.understandingReducer,
+      support: this.props.reduxStore.supportReducer,
+      comment: this.props.reduxStore.commentReducer
     }
+  };
+
+  componentDidMount() {
+    console.log(this.state.feedback)
+  }
+
+  handleSubmit = (event) => {
+      axios.post('/feedback', this.state.feedback)
+      .then(response => {
+          console.log('successful post of object:', this.state.feedback)
+      })
+      .catch(error => {
+          console.log('there was an error in the client side POST', error)
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>REVIEW YOUR FEEDBACK</h1>
+        <h3>Feelings: {this.props.reduxStore.feelingsReducer}</h3>
+        <h3>Understanding: {this.props.reduxStore.understandingReducer}</h3>
+        <h3>Support: {this.props.reduxStore.supportReducer}</h3>
+        <h3>Comments: {this.props.reduxStore.commentReducer}</h3>
+        <button>Submit</button>
+      </div>
+    );
+  }
 }
 
 
